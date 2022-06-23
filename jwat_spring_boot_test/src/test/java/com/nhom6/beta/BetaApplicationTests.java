afd2408cd6739e0318f474bb71b42fa3c732fb8e
hatucam <102598542+hatucam@users.noreply.github.com>
2022-06-23 18:06:14 +0700

    Add files via upload

---
+++ jwat_spring_boot_test/src/test/java/com/nhom6/beta/BetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom6.beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
